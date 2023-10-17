1ec067a6592b68bfb405cdae02733156e7d7b014
tutto-bello <muller.gyorgy.gergely@gmail.com>
2023-10-17 12:34:56 +0200

    fix(database): save data to localStorage

--- src/pages/api/asset/utils/database.ts
+++ src/pages/api/asset/utils/database.ts
@@ -1,22 +1,57 @@
-import fs from "fs";
-import path from "path";
 import { IAssetsResponse } from "../../../../models/asset-type";
+import { LocalStorage } from "node-localstorage";
+import { Status } from "../../../../models/status-enum";
 
-const databasePath = path.join(process.cwd(), "/tmp", "assets.json");
+const localStorage = new LocalStorage("./tmp");
+
+const localStorageKey = "assets";
+
+const defaultAssets: IAssetsResponse[] = [
+  {
+    name: "Confluence",
+    description: "Manage software development assets",
+    quantity: "1",
+    status: Status.Pending,
+    id: "123412412",
+  },
+  {
+    name: "Github",
+    description: "Code hosting platform",
+    quantity: "3",
+    status: Status.Online,
+    id: "123412wgs2",
+  },
+  {
+    name: "Jira",
+    description: "Create issues",
+    quantity: "1",
+    status: Status.Online,
+    id: "12341sdg12",
+  },
+];
 
 export function getAssets(): IAssetsResponse[] {
   try {
-    const fileContent = fs.readFileSync(databasePath, "utf-8");
-    const assets = JSON.parse(fileContent);
+    const localStorageData = localStorage.getItem(localStorageKey);
+
+    if (!localStorageData) {
+      localStorage.setItem(localStorageKey, JSON.stringify(defaultAssets));
+      return defaultAssets;
+    }
+
+    const assets = JSON.parse(localStorageData);
     return assets;
   } catch (error) {
-    console.error("Error reading assets from file:", error);
+    console.error("Error reading assets from localStorage:", error);
     return [];
   }
 }
 
 export function saveAssets(assets: IAssetsResponse[]): void {
-  console.log(databasePath);
-  const content = JSON.stringify(assets, null, 2);
-  fs.writeFileSync(databasePath, content, "utf-8");
+  try {
+    const content = JSON.stringify(assets, null, 2);
+    localStorage.setItem(localStorageKey, content);
+  } catch (error) {
+    console.error("Error saving assets to localStorage:", error);
+  }
 }
